68708ee8db52d594a638d2e8e1d0ddededdb1712
Дмитрий Березиков <dm.berezikov@ya.ru>
2022-04-28 23:00:58 +0700

    Добавьте файлы проекта.

---
+++ HomeWork_26/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_26
+{
+    internal abstract class Client
+    {
+        string Name { get => this.name; set => this.name = value; }
+
+        uint ID { get => this.id; set => this.id = value; }
+
+        Account? Deposit { get; set; }
+
+        Account? NotDeposit { get; set; }
+
+        private string name;
+        private uint id;
+
+    }
+}

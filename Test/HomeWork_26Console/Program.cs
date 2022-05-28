78e58074f3f2ba9a75e73623b020757f0ddc7ced
Дмитрий Березиков <dm.berezikov@ya.ru>
2022-05-29 01:18:42 +0700

    Инфраструктура папок

---
+++ Test/HomeWork_26Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork_26Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

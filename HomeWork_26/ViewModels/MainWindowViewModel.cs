bfa2e23b1f615deabb39068ad1148785cda8162e
Дмитрий Березиков <dm.berezikov@ya.ru>
2022-05-30 23:04:19 +0700

    Привязка данных к статус бару

--- HomeWork_26/ViewModels/MainWindowViewModel.cs
+++ HomeWork_26/ViewModels/MainWindowViewModel.cs
@@ -18,6 +18,17 @@ namespace HomeWork_26.ViewModels
         {
             get => _Title;
             set => Set(ref _Title, value);
+        }
+        #endregion
+
+        #region Status : string - Статус программы
+        private string _Status = "Готов!";
+
+        /// <summary>Статус программы</summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
         } 
         #endregion
     }

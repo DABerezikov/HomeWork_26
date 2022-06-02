c938f56ca5ef08ecf4158bbe6afd375285da511d
Дмитрий Березиков <dm.berezikov@ya.ru>
2022-06-02 23:48:49 +0700

    Добавление в модель отображения команды создания клиента и полей с параметрами клиента и его счета

--- HomeWork_26/Models/Client.cs
+++ HomeWork_26/Models/Client.cs
@@ -56,7 +56,7 @@ namespace HomeWork_26.Models
         /// </summary>
         private string? name;
         private uint id;
-        private static uint depositRate;
+        private uint depositRate;
         private Account? deposit;
         private Account? notDeposit;
 
@@ -205,6 +205,11 @@ namespace HomeWork_26.Models
         {
             DepositRate = 6;
         }
+
+        public EntityClient(string Name, string TapeAccount) : base(Name, TapeAccount) { DepositRate = 6; }
+
+        public EntityClient(string Name, string TapeAccount, double Amount) : base(Name, TapeAccount, Amount) { DepositRate = 6; }
+
     }
 
     internal class VIPClient : Client
@@ -218,5 +223,9 @@ namespace HomeWork_26.Models
             DepositRate = 12;
 
         }
+
+        public VIPClient(string Name, string TapeAccount) : base(Name, TapeAccount) { DepositRate = 12; }
+
+        public VIPClient(string Name, string TapeAccount, double Amount) : base(Name, TapeAccount, Amount) { DepositRate = 12; }
     }
 }
--- HomeWork_26/ViewModels/MainWindowViewModel.cs
+++ HomeWork_26/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using HomeWork_26.Infrastructure.Commands;
+using HomeWork_26.Models;
+using HomeWork_26.Services;
 using HomeWork_26.ViewModels.Base;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 
@@ -7,6 +10,8 @@ namespace HomeWork_26.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        static string Path = "@_DB.json";
+
         #region Заголовок окна
 
         /// <summary>Заголовок окна</summary>
@@ -30,7 +35,98 @@ namespace HomeWork_26.ViewModels
         }
         #endregion
 
+        #region NameClient : string - Имя клиента
+        private string _NameClient = "Имя клиента";
+
+        /// <summary>Имя клиента</summary>
+        public string NameClient
+        {
+            get => _NameClient;
+            set => Set(ref _NameClient, value);
+        }
+        #endregion
+
+        #region TypeClient : string - Тип клиента
+        private string _TypeClient;
+
+        /// <summary>Тип клиента</summary>
+        public string TypeClient
+        {
+            get => _TypeClient;
+            set => Set(ref _TypeClient, value);
+        }
+        #endregion
+
+        #region TypeAccount : string - Тип счета клиента
+        private string _TypeAccount = "Deposit";
+
+        /// <summary>Тип счета клиента</summary>
+        public string TypeAccount
+        {
+            get => _TypeAccount;
+            set => Set(ref _TypeAccount, value);
+        }
+        #endregion
+
+        #region AmountClient : string - Сумма пополнения счета клиента
+        private string _AmountClient = "0";
+
+        /// <summary>Сумма пополнения счета клиента</summary>
+        public string AmountClient
+        {
+            get => _AmountClient;
+            set => Set(ref _AmountClient, value);
+        }
+        #endregion
+
+        #region DBClients : ObservableCollection - База данных клиентов банка
+        private ObservableCollection<Client> _DBClients;
+
+        /// <summary>База клиентов</summary>
+        public ObservableCollection<Client> DBClients
+        {
+            get => _DBClients;
+            set => Set(ref _DBClients, value);
+        }
+        #endregion
+
         #region Команды
+
+        #region CreateClientCommand
+        public ICommand CreateClientCommand { get; }
+
+        private bool CanCreateClientCommandExecute(object p) => true;
+        private void OnCreateClientCommandExecuted(object p)
+        {          
+            
+            DBClients.Add(new Client(NameClient, TypeAccount, double.Parse(AmountClient)));
+            LoadSave<Client>.SaveDB(Path, DBClients);
+        }
+        #endregion
+
+        #region CreateEntityClientCommand
+        public ICommand CreateEntityClientCommand { get; }
+
+        private bool CanCreateEntityClientCommandExecute(object p) => true;
+        private void OnCreateEntityClientCommandExecuted(object p)
+        {
+
+            DBClients.Add(new EntityClient(NameClient, TypeAccount, double.Parse(AmountClient)));
+            LoadSave<Client>.SaveDB(Path, DBClients);
+        }
+        #endregion
+
+        #region CreateVIPClientCommand
+        public ICommand CreateVIPClientCommand { get; }
+
+        private bool CanCreateVIPClientCommandExecute(object p) => true;
+        private void OnCreateVIPClientCommandExecuted(object p)
+        {
+
+            DBClients.Add(new VIPClient(NameClient, TypeAccount, double.Parse(AmountClient)));
+            LoadSave<Client>.SaveDB(Path, DBClients);
+        }
+        #endregion
 
         #region CloseAppicationCommand
         public ICommand CloseAppicationCommand { get; }
@@ -39,7 +135,7 @@ namespace HomeWork_26.ViewModels
         private void OnCloseAppicationCommandExecuted(object p)
         {
             Application.Current.Shutdown();
-        } 
+        }
         #endregion
 
 
@@ -51,9 +147,16 @@ namespace HomeWork_26.ViewModels
             #region Команды
 
             CloseAppicationCommand = new LambdaCommand(OnCloseAppicationCommandExecuted, CanCloseAppicationCommandExecute);
-
+            CreateClientCommand = new LambdaCommand(OnCreateClientCommandExecuted, CanCreateClientCommandExecute);
+            CreateEntityClientCommand = new LambdaCommand(OnCreateEntityClientCommandExecuted, CanCreateEntityClientCommandExecute);
+            CreateVIPClientCommand = new LambdaCommand(OnCreateVIPClientCommandExecuted, CanCreateVIPClientCommandExecute);
             #endregion
 
+            #region Данные
+
+            DBClients = LoadSave<Client>.LoadDB(Path);
+
+            #endregion
         }
     }
 

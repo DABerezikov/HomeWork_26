d6fc4fba62725c6d5d8e95cb76d03b93d2860b4a
Дмитрий Березиков <dm.berezikov@ya.ru>
2022-06-18 14:26:38 +0700

    Поиск возможности отображать данные

--- HomeWork_26/Models/Client.cs
+++ HomeWork_26/Models/Client.cs
@@ -100,7 +100,7 @@ namespace HomeWork_26.Models
         {
             switch (TypeAccount)
             {
-                case "Deposit":
+                case "Депозитный счет":
                     Deposit = new Account(ID, DepositRate, Amount);
                     break;
                 default:
@@ -122,12 +122,14 @@ namespace HomeWork_26.Models
             string? result;
             switch (TypeAccount)
             {
-                case "Deposit":
+                case "Депозитный счет":
                     result = Deposit?.Close();
+                    Deposit = null;
                     break;
 
                 default:
                     result = NotDeposit?.Close();
+                    NotDeposit = null;
                     break;
 
             }
